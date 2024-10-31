29daf0734bcd99570632d61f34e5fa31de3a943c
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-10-31 08:50:35 +0530

    Adding oop folder for the Object oriented programming

---
+++ OOP/oop.java
@@ -0,0 +1,21 @@
+package OOP;
+
+public class oop {
+    public static void main(String args[]){
+        pen p = new pen();
+        p.colour("blue");
+        p.length(5);
+        System.out.println("Colour: " + p.colour + " Length: " + p.length);
+    }
+}
+ class pen {
+    String colour;
+    int length;
+    void colour(String c) {
+        colour = c;
+    }
+    void length(int l) {
+        length = l;
+    }
+
+ }
