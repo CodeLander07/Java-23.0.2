191c3863c7a3ccdfcbe80b585ff382884e15d584
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-10 21:15:05 +0530

    Refactor ComplexNum class to improve functionality with addition, subtraction, and multiplication methods; add main method for testing operations.

--- OOP/ComplexNum.java
+++ OOP/ComplexNum.java
@@ -1,21 +1,54 @@
 package OOP;
-import java.util.*;
-public class ComplexNum {
-	int real ;
+
+public class ComplexNum{
+    int real;
     int img;
 
-    // consturtor fuction to initialize the values
-    public complex(int real , int img){
-        this.real = real;
-        this.img = img;
+    public Complex(int r, int i) {
+        real = r;
+        img = i;
     }
-    public void print(){
-        System.out.println(this.real + " + i" +this.img);
+
+    // function to add two complex numbers
+    public static Complex add(Complex c1, Complex c2) {
+        Complex c = new Complex(0, 0);
+        c.real = c1.real + c2.real;
+        c.img = c1.img + c2.img;
+        return c;
     }
-    
-    public static void add(ComplexNum c1 , ComplexNum c2){
-        int real = c1.real + c2.real;
-        int img = c1.img + c2.img;
-        System.out.println(real + " + i" + img);
+
+    // function to subtract two complex numbers
+    public static Complex sub(Complex c1, Complex c2) {
+        Complex c = new Complex(0, 0);
+        c.real = c1.real - c2.real;
+        c.img = c1.img - c2.img;
+        return c;
+    }
+
+    // function to multiply two complex numbers
+    public static Complex mul(Complex c1, Complex c2) {
+        Complex c = new Complex(0, 0);
+        c.real = c1.real * c2.real - c1.img * c2.img;
+        c.img = c1.real * c2.img + c1.img * c2.real;
+        return c;
+    }
+
+    // function to print the complex number
+    public static void print(Complex c) {
+        System.out.println(c.real + " + i" + c.img);
     }
 }
+
+class Solution {
+    public static void main(String[] args) {
+        Complex c1 = new Complex(3, 2);
+        Complex c2 = new Complex(1, 7);
+        Complex c = Complex.add(c1, c2);
+        Complex.print(c);
+        c = Complex.sub(c1, c2);
+        Complex.print(c);
+        c = Complex.mul(c1, c2);
+        Complex.print(c);
+    }
+}
+
