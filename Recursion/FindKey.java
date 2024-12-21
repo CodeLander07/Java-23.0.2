5c397d6d6d78081b6855442a0610cfe9de6fd705
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-21 21:18:31 +0530

    Refactor myAtoi method to improve readability and add FindKey class with recursive method for key search in an array

---
+++ Recursion/FindKey.java
@@ -0,0 +1,12 @@
+public class FindKey {
+    public static int findkey(int[] arr, int key, int index) {
+        
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1,2,5,4,7,2,2,6,2};
+        int key = 2;
+        int index = arr.length - 1;
+        System.out.println(findkey(arr, key, index));
+    }
+}
