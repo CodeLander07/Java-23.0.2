2092021e75316cfec91227affae362dffbeb663f
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-21 21:29:46 +0530

    Refactor FindKey class to improve key search logic and update main method for clarity

--- Recursion/FindKey.java
+++ Recursion/FindKey.java
@@ -1,12 +1,19 @@
 public class FindKey {
     public static int findkey(int[] arr, int key, int index) {
+        if (index >= arr.length) {
+            return -1;
+        }
+        if(arr[index] == key){
+            System.out.print(index + " "); 
+        }
+        return findkey(arr, key , index + 1 );
         
     }
 
     public static void main(String[] args){
         int arr[] = {1,2,5,4,7,2,2,6,2};
         int key = 2;
-        int index = arr.length - 1;
-        System.out.println(findkey(arr, key, index));
+        int index = 0;
+        findkey(arr, key, index);
     }
 }
