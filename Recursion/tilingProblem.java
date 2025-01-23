2094427ecd81cc16755a37e9af3cb45bfcaa05bc
unstopablesid <mayurnikumbh2004@gmail.com>
2025-01-23 22:23:05 +0530

    changing values

--- Recursion/tilingProblem.java
+++ Recursion/tilingProblem.java
@@ -15,8 +15,8 @@ class TilingProblem {
 
         int[] dp = new int[n + 1];
         dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+        dp[1] = 5;
+        dp[2] = 6;
 
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
