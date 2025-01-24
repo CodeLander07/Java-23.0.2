665a42352979477dbd29dad4b8ec5aa4246b62d5
unstopablesid <mayurnikumbh2004@gmail.com>
2025-01-24 07:41:31 +0530

    Refactor RemoveDuplicates class to implement recursive method for removing duplicates from a string and update main method for testing

--- Recursion/RemoveDuplicates.java
+++ Recursion/RemoveDuplicates.java
@@ -1,17 +1,29 @@
 public class RemoveDuplicates{
-    public static String removeDuplicates(String s) {
-        if (s.length() == 0) {
-            return "";
+    /**
+     * Removes duplicate characters from the string.
+     * @param s the input string
+     * @param idx the current index in the string
+     * @param sb the StringBuilder to store the result
+     * @param charMap the boolean array to track characters
+     */
+    public static void removeDuplicates(String s, int idx, StringBuilder sb, boolean[] charMap) {
+        if (idx == s.length()) {
+            System.out.println(sb.toString());
+            return;
         }
-        if (s.length() == 1) {
-            return s;
+        // Check if the current character is already in the charMap
+        char currChar = s.charAt(idx);
+        if (charMap[currChar - 'a']) {
+            removeDuplicates(s, idx + 1, sb, charMap);
+        } else {
+            charMap[currChar - 'a'] = true;
+            removeDuplicates(s, idx + 1, new StringBuilder(sb).append(currChar), charMap);
         }
-        if (s.charAt(0) == s.charAt(1)) {
-            return removeDuplicates(s.substring(1));
-        }
-        return s.charAt(0) + removeDuplicates(s.substring(1));
     }
     public static void main(String[] args) {
-        System.out.println(removeDuplicates("aaaabbbcc"));
+        String s = "MayurShantaramPatil".toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        boolean charMap[] = new boolean[128];
+        removeDuplicates(s,0,sb,charMap);
     }
 }
