f874a7d5d156ab14106668ee581d700179cf1e9e
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-05-14 15:41:31 +0530

    Add checkLLisPelindrome class to check if a linked list is a palindrome

--- Stack/checkLLisPelindrome.java
+++ Stack/checkLLisPelindrome.java
@@ -1,3 +1,33 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class checkLLisPelindrome {
-    
+    public static boolean isPalindrome(LinkedList<Integer> list) {
+        
+        Stack<Integer> stack = new Stack<>();
+
+        
+        for (int num : list) {
+            stack.push(num);
+        }
+
+        for (int num : list) {
+            if (num != stack.pop()) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(1);
+
+        System.out.println("Is palindrome? " + isPalindrome(list)); 
+    }
 }
