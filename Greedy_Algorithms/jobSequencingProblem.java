81c69f90b13aff710156815fc227138b7dcb6d73
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-05-30 12:28:25 +0530

    Implement job sequencing algorithm and add chocolate cutting cost calculation

--- Greedy_Algorithms/jobSequencingProblem.java
+++ Greedy_Algorithms/jobSequencingProblem.java
@@ -9,35 +9,47 @@ public class jobSequencingProblem {
         int deadline;
         int profit;
 
-        Job(int id, int deadline, int profit) {
-            this.id = id;
-            this.deadline = deadline;
-            this.profit = profit;
+        public Job(int i, int d, int p) {
+            id = i;
+            deadline = d;
+            profit = p;
         }
     }
-        public static void main(String [] args ){
-            int jobsInfo [][] = {{4,20},{1,10},{1,40},{1,30}};
 
-            ArrayList <Job> jobs= new ArrayList<>();
+    public static void main(String[] args) {
+        int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {5, 30}};
+        Job[] jobs = new Job[jobsInfo.length];
+        for (int i = 0; i < jobs.length; i++) {
+            jobs[i] = new Job(i, jobsInfo[i][0], jobsInfo[i][1]);
+        }
+        Arrays.sort(jobs, (a, b) -> b.profit - a.profit);
 
-            for(int i = 0; i<jobsInfo.length; i++){
-                jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+        // Find max deadline to determine the number of slots
+        int maxDeadline = 0;
+        for (Job job : jobs) {
+            if (job.deadline > maxDeadline) {
+                maxDeadline = job.deadline;
             }
-            Collections.sort(jobs , (a,b) -> b.profit - a.profit);
-            ArrayList <Integer> seq = new ArrayList<>();
-            int Time = 0;
-            for(int i=0; i<jobs.size();i++){
-                Job curr = jobs.get(i);
-                if(curr.deadline >Time){
-                    seq.add(curr.id);
-                    Time += curr.deadline;
+        }
+
+        int[] slots = new int[maxDeadline];
+        Arrays.fill(slots, -1);
+        int totalProfit = 0;
+        int countJobs = 0;
+
+        for (Job job : jobs) {
+            // Find a free slot for this job (starting from the last possible slot)
+            for (int j = job.deadline - 1; j >= 0; j--) {
+                if (slots[j] == -1) {
+                    slots[j] = job.id;
+                    totalProfit += job.profit;
+                    countJobs++;
+                    break;
                 }
-
             }
-            System.out.println("Max jobs = " + seq.size());
+        }
 
-            for(int i=0; i<seq.size(); i++){
-                System.out.print(seq.get(i) + " ");
-            }
-        }    
+        System.out.println("Max jobs: " + countJobs);
+        System.out.println("Total profit: " + totalProfit);
+    }
 }
