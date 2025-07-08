06e0b56960f5994b3ed066eddee64379d94c4e17
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-07-08 14:22:06 +0530

    Add minDepth and isBalance methods to BinaryTrees class

--- BinaryTrees/BinaryTrees.java
+++ BinaryTrees/BinaryTrees.java
@@ -38,6 +38,36 @@ public class BinaryTrees{
 
         return leftSum + rightSum + root.val;
     }
+    public static int minDepth(Node root){
+
+        //base case
+        if(root == null) return 0;
+
+        //checks if the node is a leaf 
+        int ld = minDepth(root.left);
+        //check if the node is leaf
+        int rd = minDepth(root.right);
+
+        //if both left and right are null, return 1
+        if(root.left == null) return rd+1;
+        if(root.right == null) return ld+1;
+
+
+        return Math.min(ld,rd) + 1;
+    }
+
+    public static boolean isBalance(Node root){
+        if(root == null) return true;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        //check if the height difference is more than 1
+        if(Math.abs(leftHeight - rightHeight) > 1) return false;
+        
+
+        return isBalance(root.left) && isBalance(root.right);
+    }
+    
 public static void main(String[] args){
     Node a = new Node(1);
     Node b = new Node(2);
@@ -56,6 +86,8 @@ public static void main(String[] args){
     System.out.println(height(a));
     System.out.println(countNodes(a));
     System.out.println(sumOfNodes(a));
+    System.out.println(minDepth(a));
+    System.out.println(isBalance(a));
 
 } 
 }
